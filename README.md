1579aa95fd5bd989de25875575ed348d5e6f98e4
daniel dabrowski <agrcom@yahoo.pl>
2018-04-30 22:09:41 +0200

    Dockerized

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # RegistrationForm
 Simple REST project, main purpose is check possibility of Kotlin and Spring Boot
+
+#Docker commands
+create docker image:
+
+    mvn install dockerfile:build
+    
+run app: 
+    
+    docker run -p 8080:8080 -t agrcom/registration.form
--- src/main/kotlin/com/daniel/dabrowski/registration/UserController.kt
+++ src/main/kotlin/com/daniel/dabrowski/registration/UserController.kt
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class UserController {
 
+    @GetMapping("/")
+    fun homeEndpoint() : String{
+        return "Hello!"
+    }
+
     @GetMapping("/demo")
     fun demoEndpoint(@RequestParam(value = "name", defaultValue = "World") name: String,
                      @RequestParam(value = "password", defaultValue = "dupa123") password: String,
